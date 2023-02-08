fb2475b83ce52f7ba81bb25ba03fd44e38611c55
blackvccat <1293720759@qq.com>
2023-02-09 00:39:25 +0800

    喵

--- src/main/java/io/github/blackvccat/vcgun_mod/Main.java
+++ src/main/java/io/github/blackvccat/vcgun_mod/Main.java
@@ -1,6 +1,4 @@
-package com.example.vcgun_mod;
-
-
+package io.github.blackvccat.vcgun_mod;
 import net.minecraft.Util;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
